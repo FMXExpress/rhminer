5515883c3bf572877b32d24dbc5f3cd4d21a82ac
maxirmx <maxim@samsonov.net>
2021-02-24 20:07:30 +0300

    No 3

---
+++ rhminer/resource-gcc.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by rhminer.rc
+//
+#define IDC_MYICON                      2
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDI_PASCALCOINSCREENSAVER       107
+#define IDI_SMALL                       108
+#define IDC_PASCALCOINSCREENSAVER       109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDB_BITMAP1                     131
+#define IDB_BITMAP2                     132
+#define IDB_BITMAP3                     133
+#define IDB_BITMAP_PASSIVE              133
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_STATIC10                    1001
+#define IDC_EDIT3                       1002
+#define IDC_EDIT4                       1003
+#define IDC_CHECK_NONCE                 1004
+#define IDC_COMBO1                      1005
+#define DLG_SCRNSAVECONFIGURE           2003
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
